3538331806a1a649f5036260b6addf0235735b93
SurfaceNote <surfacenote@gmail.com>
2025-03-03 00:13:14 +0100

    Api: added new origins

--- MyStreamHistory.API/Program.cs
+++ MyStreamHistory.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200") 
+            policy.WithOrigins("http://localhost", "http://mystreamhistory.com", "http://mystreamhistory.com") 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
