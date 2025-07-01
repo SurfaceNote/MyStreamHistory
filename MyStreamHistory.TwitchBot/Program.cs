6a1ffa9ecebfec007944b98bf94522654cd8b86f
SurfaceNote <surfacenote@gmail.com>
2025-07-02 01:35:42 +0700

    TwitchBot: added basic Twitch IRC connection

--- MyStreamHistory.TwitchBot/Program.cs
+++ MyStreamHistory.TwitchBot/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Sentry.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MyStreamHistory.TwitchBot.Services;
+
+try
+{
+    var host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, services) =>
+        {
+            services.AddHostedService<TwitchBotService>();
+            services.AddSingleton<TwitchIrcService>();
+            services.AddHttpClient("TwitchApi", client =>
+            {
+                client.BaseAddress = new Uri("https://api.twitch.tv/helix/");
+                client.DefaultRequestHeaders.Add("Client-ID", "");
+            });
+        })
+        .UseSerilog()
+        .Build();
+    await host.RunAsync();
+}
+catch
+{
+
+}
