7dd509bfe88e1d7aed059cb1cb372d2a91813363
SurfaceNote <surfacenote@gmail.com>
2025-09-13 20:47:46 +0700

    TwitchBot: Adds project TwitchBot (#9)

---
+++ MyStreamHistory.TwitchBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Headers;
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+Env.Load();
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddHttpClient("TwitchApi", c =>
+{
+   c.BaseAddress = new Uri("https://api.twitch.tv/helix/");
+   c.DefaultRequestHeaders.Add("Client-ID", "");
+   c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
+});
+
+builder.Services.AddHttpClient("Backend", c =>
+{
+   c.BaseAddress = new Uri("");
+});
+
+await builder.Build().RunAsync();
