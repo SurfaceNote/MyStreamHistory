ad60851dbe563a4d02b646cd8b7987c0ea0f7d08
SurfaceNote <surfacenote@gmail.com>
2025-09-14 00:43:15 +0700

    TwitchBot: adds a service for receiving and updating OAuth tokens (#11)

--- MyStreamHistory.TwitchBot/Program.cs
+++ MyStreamHistory.TwitchBot/Program.cs
@@ -3,16 +3,29 @@ using DotNetEnv;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyStreamHistory.TwitchBot.Services;
+using Serilog;
+using Serilog.Events;
+
+Env.Load();
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-Env.Load();
 builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddSerilog(config =>
+{
+   config.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+         .Enrich.FromLogContext()
+         .Enrich.WithProperty("Application", "TwitchBot");
+   
+   config.WriteTo.Console();
+});
+
 
 builder.Services.AddHttpClient("TwitchApi", c =>
 {
    c.BaseAddress = new Uri("https://api.twitch.tv/helix/");
-   c.DefaultRequestHeaders.Add("Client-ID", "");
    c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
 });
 
@@ -20,5 +33,7 @@ builder.Services.AddHttpClient("Backend", c =>
 {
    c.BaseAddress = new Uri("");
 });
+
+builder.Services.AddSingleton<TwitchAuthService>();
 
 await builder.Build().RunAsync();
