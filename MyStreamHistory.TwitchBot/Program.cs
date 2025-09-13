5684207303bacddc6557f881c646a2eb94d9d8f8
SurfaceNote <surfacenote@gmail.com>
2025-09-13 20:42:05 +0700

    TwitchBot: creates empty project for TwitchBot

--- MyStreamHistory.TwitchBot/Program.cs
+++ MyStreamHistory.TwitchBot/Program.cs
@@ -1,29 +1,24 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Net.Http.Headers;
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Sentry.AspNetCore;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
-using MyStreamHistory.TwitchBot.Services;
 
-try
-{
-    var host = Host.CreateDefaultBuilder(args)
-        .ConfigureServices((context, services) =>
-        {
-            services.AddHostedService<TwitchBotService>();
-            services.AddSingleton<TwitchIrcService>();
-            services.AddHttpClient("TwitchApi", client =>
-            {
-                client.BaseAddress = new Uri("https://api.twitch.tv/helix/");
-                client.DefaultRequestHeaders.Add("Client-ID", "");
-            });
-        })
-        .UseSerilog()
-        .Build();
-    await host.RunAsync();
-}
-catch
-{
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-}
+Env.Load();
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddHttpClient("TwitchApi", c =>
+{
+   c.BaseAddress = new Uri("https://api.twitch.tv/helix/");
+   c.DefaultRequestHeaders.Add("Client-ID", "");
+   c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
+});
+
+builder.Services.AddHttpClient("Backend", c =>
+{
+   c.BaseAddress = new Uri("");
+});
+
+await builder.Build().RunAsync();
