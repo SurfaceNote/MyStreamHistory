73607aa74ce64e8cd4163858e42e3f0b03b3a9d8
SurfaceNote <surfacenote@gmail.com>
2025-11-17 03:11:19 +0700

    Shared: Added Persistence

---
+++ Backend/Shared/MyStreamHistory.Shared.Infrastructure/Persistence/Repository/RepositoryRegistrationExtenstions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyStreamHistory.Shared.Application.Repository;
+
+namespace MyStreamHistory.Shared.Infrastructure.Persistence.Repository
+{
+    public static class RepositoryRegistrationExtenstions
+    {
+        public static InfrastructureBuilder AddRepository<TEntity, TDbContext>(this InfrastructureBuilder builder)
+            where TDbContext : DbContext
+            where TEntity : class
+        {
+            builder.Services.AddScoped<IRepositoryBase<TEntity>, RepositoryBase<TEntity, TDbContext>>();
+            return builder;
+        }
+    }
+}
