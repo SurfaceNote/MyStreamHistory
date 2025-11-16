817014a2685fec19af86b1ea406912064fdf8ddb
SurfaceNote <surfacenote@gmail.com>
2025-11-16 22:59:26 +0700

    Gateway: Add more services

--- Backend/Gateway/MyStreamHistory.Gateway.Api/Program.cs
+++ Backend/Gateway/MyStreamHistory.Gateway.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi;
 using MyStreamHistory.Gateway.Api.Extenstions;
 using MyStreamHistory.Shared.Infrastructure;
 using MyStreamHistory.Shared.Infrastructure.Logging;
@@ -10,8 +11,33 @@ builder.Services.AddInfrastructure(builder.Configuration)
     .AddMassTransit()
     .AddTransportBus();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Load JWT Token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
